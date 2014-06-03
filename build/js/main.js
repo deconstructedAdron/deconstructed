1425aaa679c36ae7830da33591ef946be18053f6
Adron Hall <adronhall@gmail.com>
2014-06-02 19:23:55 -0700

    Updated to have to appropriate domain name.

--- build/js/main.js
+++ build/js/main.js
@@ -1,160 +1,158 @@
 /*global jQuery:false */
-jQuery (document).ready (function ($) {
+jQuery(document).ready(function ($) {
     "use strict";
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  0. FIX FOR CHROME BUG NOT RENDERING GOOGLE WEBFONTS
-     /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*  0. FIX FOR CHROME BUG NOT RENDERING GOOGLE WEBFONTS
+/*-----------------------------------------------------------------------------------*/
 
-    $ (function () {
-        $ ('body').hide ().show ();
+    $(function() {
+        $('body').hide().show(); 
     });
+    
+/*-----------------------------------------------------------------------------------*/
+/*  01. FITVIDS
+/*-----------------------------------------------------------------------------------*/
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  01. FITVIDS
-     /*-----------------------------------------------------------------------------------*/
+    $(".fitvid").fitVids();
 
-    $ (".fitvid").fitVids ();
+/*-----------------------------------------------------------------------------------*/
+/*	02. FLEXSLIDER SETTINGS
+/*-----------------------------------------------------------------------------------*/
 
-    /*-----------------------------------------------------------------------------------*/
-    /*	02. FLEXSLIDER SETTINGS
-     /*-----------------------------------------------------------------------------------*/
-
-    $ (".flexslider").flexslider ({
-        animation   : 'fade',
-        touch       : true,
-        slideshow   : true,
-        controlNav  : true,
+    $(".flexslider").flexslider({
+        animation: 'fade',
+        touch: true,
+        slideshow: true,
+		controlNav: true,
         directionNav: false,
         smoothHeight: true
     });
 
-    $ (".flexslider-small").flexslider ({
-        animation   : 'fade',
-        touch       : true,
-        slideshow   : true,
-        controlNav  : true,
+    $(".flexslider-small").flexslider({
+        animation: 'fade',
+        touch: true,
+        slideshow: true,
+        controlNav: true,
         directionNav: false,
         smoothHeight: true
     });
 
-    $ (".flexslider-blockquote").flexslider ({
-        animation   : 'fade',
-        touch       : true,
-        slideshow   : true,
-        controlNav  : true,
+    $(".flexslider-blockquote").flexslider({
+        animation: 'fade',
+        touch: true,
+        slideshow: true,
+        controlNav: true,
         directionNav: false,
         smoothHeight: true
     });
 
-    /*-----------------------------------------------------------------------------------*/
-    /*	03. RESPONSIVE MENU
-     /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*	03. RESPONSIVE MENU
+/*-----------------------------------------------------------------------------------*/
 
-    $ ("#collapse").hide ();
-    $ ("#collapse-menu").toggle (function () {
+		$("#collapse").hide();
+		$("#collapse-menu").toggle(function () {
+		
+			jQuery("#collapse").slideToggle(300);
+			return false;
 
-        jQuery ("#collapse").slideToggle (300);
-        return false;
+		}, function () {
 
-    }, function () {
+			$("#collapse").slideToggle(300);
+			return false;
+		});
 
-        $ ("#collapse").slideToggle (300);
-        return false;
-    });
+/*-----------------------------------------------------------------------------------*/
+/*  04. NAVIGATION
+/*-----------------------------------------------------------------------------------*/
+       
+		$('.nav a, #downed').click(function() {
+			$.scrollTo($(this).attr('href'), {duration: 500, offset:-80});
+			return false;
+		});
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  04. NAVIGATION
-     /*-----------------------------------------------------------------------------------*/
-
-    $ ('.nav a, #downed').click (function () {
-        $.scrollTo ($ (this).attr ('href'), {duration: 500, offset: -80});
-        return false;
-    });
-
-    /*-----------------------------------------------------------------------------------*/
-    /*  05. NAVIGATION EFFECT FADE IN ON SCROLL
-     /*-----------------------------------------------------------------------------------*/
-
-    $ (window).scroll (function () {
-        var scroll = $ (window).scrollTop ();
+/*-----------------------------------------------------------------------------------*/
+/*  05. NAVIGATION EFFECT FADE IN ON SCROLL
+/*-----------------------------------------------------------------------------------*/
+       
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
         if (scroll >= 400) {
-            $ (".header-shrink").fadeIn ("fast");
+            $(".header-shrink").fadeIn("fast");
         } else {
-            $ (".header-shrink").fadeOut ("fast");
+            $(".header-shrink").fadeOut("fast");
         }
     });
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  06. SLIDE IN ELEMENTS EFFECT
-     /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*  06. SLIDE IN ELEMENTS EFFECT
+/*-----------------------------------------------------------------------------------*/
+       
+    new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
 
-    new cbpScroller (document.getElementById ('cbp-so-scroller'));
-
-    /*-----------------------------------------------------------------------------------*/
-    /*  07. NAVIGATION WAYPOINTS
-     /*-----------------------------------------------------------------------------------*/
-
-    var sections = jQuery ("section");
-    var navigation_links = jQuery (".nav a");
-
-    sections.waypoint ({
-        handler: function (event, direction) {
-
+/*-----------------------------------------------------------------------------------*/
+/*  07. NAVIGATION WAYPOINTS
+/*-----------------------------------------------------------------------------------*/
+       
+    var sections = jQuery("section");
+    var navigation_links = jQuery(".nav a");
+    
+    sections.waypoint({
+        handler: function(event, direction) {
+        
             var active_section;
-            active_section = jQuery (this);
-            if (direction === "up") {
-                active_section = active_section.prev ();
-            }
+            active_section = jQuery(this);
+            if (direction === "up") { active_section = active_section.prev(); }
 
-            var active_link = jQuery ('.nav a[href="#' + active_section.attr ("id") + '"]');
-            navigation_links.removeClass ("selected");
-            active_link.addClass ("selected");
+            var active_link = jQuery('.nav a[href="#' + active_section.attr("id") + '"]');
+            navigation_links.removeClass("selected");
+            active_link.addClass("selected");
 
         },
-        offset : '30'
+        offset: '30'
     });
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  08. SCROLL TO TOP BUTTON ON NAVIGATION HEADER
-     /*-----------------------------------------------------------------------------------*/
-
-    $ ('.totop').click (function () {
-        $ ('html, body').animate ({
+/*-----------------------------------------------------------------------------------*/
+/*  08. SCROLL TO TOP BUTTON ON NAVIGATION HEADER
+/*-----------------------------------------------------------------------------------*/
+     
+    $('.totop').click(function () { 
+        $('html, body').animate({
             scrollTop: 0
         }, 600);
         return false;
     });
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  09. LIGHTBOX POPUP FOR GALLERY
-     /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*  09. LIGHTBOX POPUP FOR GALLERY
+/*-----------------------------------------------------------------------------------*/
+ 
+    $('.gallery-item a').has('img').attr('rel', 'lightbox');
 
-    $ ('.gallery-item a').has ('img').attr ('rel', 'lightbox');
-
-    /*-----------------------------------------------------------------------------------*/
-    /*  09. SCROLLING OPACITY OF INTRO TEXT
-     /*-----------------------------------------------------------------------------------*/
-
-    var divs = $ ('#fadeOut');
-    $ (window).on ('scroll', function () {
-        var st = $ (this).scrollTop ();
-        divs.css ({ 'opacity': (1 - st / 350) });
+/*-----------------------------------------------------------------------------------*/
+/*  09. SCROLLING OPACITY OF INTRO TEXT
+/*-----------------------------------------------------------------------------------*/
+ 
+    var divs = $('#fadeOut');
+    $(window).on('scroll', function() {
+        var st = $(this).scrollTop();
+    divs.css({ 'opacity' : (1 - st/350) });
     });
 
-    /*-----------------------------------------------------------------------------------*/
-    /*  10. ANIMATION OF GALLERY
-     /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+/*  10. ANIMATION OF GALLERY
+/*-----------------------------------------------------------------------------------*/
+ 
+    $( ".gallery-size-thumbnail" ).addClass( "cbp-so-side-img cbp-so-side-bottom" ); 
 
-    $ (".gallery-size-thumbnail").addClass ("cbp-so-side-img cbp-so-side-bottom");
-
-    /*-----------------------------------------------------------------------------------*/
-    /*  11. PARALLAX EFFECT HEADER & FOOTER
-     /*-----------------------------------------------------------------------------------*/
-
-    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test (navigator.userAgent)) {
-        $ ('#header').parallax ("0%", 0.5);
-        $ ('#footer').parallax ("0%", 0.5);
-    }
+/*-----------------------------------------------------------------------------------*/
+/*  11. PARALLAX EFFECT HEADER & FOOTER
+/*-----------------------------------------------------------------------------------*/
+ 
+if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('#header').parallax("0%", 0.5); 
+    $('#footer').parallax("0%", 0.5); 
+} 
 
 });
