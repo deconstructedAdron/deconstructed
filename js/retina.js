083ad58462bbb6d7d511ae979bdcd900e6459532
Adron Hall <adronhall@gmail.com>
2014-06-02 19:26:04 -0700

    Added the domain name appropriately to the build for prod.

--- js/retina.js
+++ js/retina.js
@@ -1,43 +1,6 @@
-// retina.js, a high-resolution image swapper (http://retinajs.com), v0.0.2
+ // retina.js, a high-resolution image swapper (http://retinajs.com), v0.0.2
 
-(function () {
-    function t (e) {
-        this.path = e;
-        var t = this.path.split ("."), n = t.slice (0, t.length - 1).join ("."), r = t[t.length - 1];
-        this.at_2x_path = n + "@2x." + r
-    }
-
-    function n (e) {
-        this.el = e, this.path = new t (this.el.getAttribute ("src"));
-        var n = this;
-        this.path.check_2x_variant (function (e) {
-            e && n.swap ()
-        })
-    }
-
-    var e = typeof exports == "undefined" ? window : exports;
-    e.RetinaImagePath = t, t.confirmed_paths = [], t.prototype.is_external = function () {
-        return!!this.path.match (/^https?\:/i) && !this.path.match ("//" + document.domain)
-    }, t.prototype.check_2x_variant = function (e) {
-        var n, r = this;
-        if (this.is_external ())return e (!1);
-        if (this.at_2x_path in t.confirmed_paths)return e (!0);
-        n = new XMLHttpRequest, n.open ("HEAD", this.at_2x_path), n.onreadystatechange = function () {
-            return n.readyState != 4 ? e (!1) : n.status >= 200 && n.status <= 399 ? (t.confirmed_paths.push (r.at_2x_path), e (!0)) : e (!1)
-        }, n.send ()
-    }, e.RetinaImage = n, n.prototype.swap = function (e) {
-        function n () {
-            t.el.complete ? (t.el.setAttribute ("width", t.el.offsetWidth), t.el.setAttribute ("height", t.el.offsetHeight), t.el.setAttribute ("src", e)) : setTimeout (n, 5)
-        }
-
-        typeof e == "undefined" && (e = this.path.at_2x_path);
-        var t = this;
-        n ()
-    }, e.devicePixelRatio > 1 && (window.onload = function () {
-        var e = document.getElementsByTagName ("img"), t = [], r, i;
-        for (r = 0; r < e.length; r++)i = e[r], t.push (new n (i))
-    })
-}) ();
+(function(){function t(e){this.path=e;var t=this.path.split("."),n=t.slice(0,t.length-1).join("."),r=t[t.length-1];this.at_2x_path=n+"@2x."+r}function n(e){this.el=e,this.path=new t(this.el.getAttribute("src"));var n=this;this.path.check_2x_variant(function(e){e&&n.swap()})}var e=typeof exports=="undefined"?window:exports;e.RetinaImagePath=t,t.confirmed_paths=[],t.prototype.is_external=function(){return!!this.path.match(/^https?\:/i)&&!this.path.match("//"+document.domain)},t.prototype.check_2x_variant=function(e){var n,r=this;if(this.is_external())return e(!1);if(this.at_2x_path in t.confirmed_paths)return e(!0);n=new XMLHttpRequest,n.open("HEAD",this.at_2x_path),n.onreadystatechange=function(){return n.readyState!=4?e(!1):n.status>=200&&n.status<=399?(t.confirmed_paths.push(r.at_2x_path),e(!0)):e(!1)},n.send()},e.RetinaImage=n,n.prototype.swap=function(e){function n(){t.el.complete?(t.el.setAttribute("width",t.el.offsetWidth),t.el.setAttribute("height",t.el.offsetHeight),t.el.setAttribute("src",e)):setTimeout(n,5)}typeof e=="undefined"&&(e=this.path.at_2x_path);var t=this;n()},e.devicePixelRatio>1&&(window.onload=function(){var e=document.getElementsByTagName("img"),t=[],r,i;for(r=0;r<e.length;r++)i=e[r],t.push(new n(i))})})();
 
 /*
  * jQuery Quovolver v1.0 - http://sandbox.sebnitu.com/jquery/quovolver
@@ -46,66 +9,66 @@
  * 
  */
 
-(function ($) {
-    $.fn.quovolver = function (speed, delay) {
-
-        /* Sets default values */
-        if (!speed) speed = 500;
-        if (!delay) delay = 6000;
-
-        // If "delay" is less than 4 times the "speed", it will break the effect
-        // If that's the case, make "delay" exactly 4 times "speed"
-        var quaSpd = (speed * 4);
-        if (quaSpd > (delay)) delay = quaSpd;
-
-        // Create the variables needed
-        var quote = $ (this),
-            firstQuo = $ (this).filter (':first'),
-            lastQuo = $ (this).filter (':last'),
-            wrapElem = '<div id="quote_wrap"></div>';
-
-        // Wrap the quotes
-        $ (this).wrapAll (wrapElem);
-
-        // Hide all the quotes, then show the first
-        $ (this).hide ();
-        $ (firstQuo).show ();
-
-        // Set the hight of the wrapper
-        $ (this).parent ().css ({height: $ (firstQuo).height ()});
-
-        // Where the magic happens
-        setInterval (function () {
-
-            // Set required hight and element in variables for animation
-            if ($ (lastQuo).is (':visible')) {
-                var nextElem = $ (firstQuo);
-                var wrapHeight = $ (nextElem).height ();
-            } else {
-                var nextElem = $ (quote).filter (':visible').next ();
-                var wrapHeight = $ (nextElem).height ();
-            }
-
-            // Fadeout the quote that is currently visible
-            $ (quote).filter (':visible').fadeOut (speed);
-
-            // Set the wrapper to the hight of the next element, then fade that element in
-            setTimeout (function () {
-                $ (quote).parent ().animate ({height: wrapHeight}, speed);
-            }, speed);
-
-            if ($ (lastQuo).is (':visible')) {
-                setTimeout (function () {
-                    $ (firstQuo).fadeIn (speed * 2);
-                }, speed * 2);
-
-            } else {
-                setTimeout (function () {
-                    $ (nextElem).fadeIn (speed);
-                }, speed * 2);
-            }
-
-        }, delay);
-
-    };
-}) (jQuery);
+(function($) {
+	$.fn.quovolver = function(speed, delay) {
+		
+		/* Sets default values */
+		if (!speed) speed = 500;
+		if (!delay) delay = 6000;
+		
+		// If "delay" is less than 4 times the "speed", it will break the effect 
+		// If that's the case, make "delay" exactly 4 times "speed"
+		var quaSpd = (speed*4);
+		if (quaSpd > (delay)) delay = quaSpd;
+		
+		// Create the variables needed
+		var	quote = $(this),
+			firstQuo = $(this).filter(':first'),
+			lastQuo = $(this).filter(':last'),
+			wrapElem = '<div id="quote_wrap"></div>';
+		
+		// Wrap the quotes
+		$(this).wrapAll(wrapElem);
+		
+		// Hide all the quotes, then show the first
+		$(this).hide();
+		$(firstQuo).show();
+		
+		// Set the hight of the wrapper
+		$(this).parent().css({height: $(firstQuo).height()});		
+		
+		// Where the magic happens
+		setInterval(function(){
+			
+			// Set required hight and element in variables for animation
+			if($(lastQuo).is(':visible')) {
+				var nextElem = $(firstQuo);
+				var wrapHeight = $(nextElem).height();
+			} else {
+				var nextElem = $(quote).filter(':visible').next();
+				var wrapHeight = $(nextElem).height();
+			}
+			
+			// Fadeout the quote that is currently visible
+			$(quote).filter(':visible').fadeOut(speed);
+			
+			// Set the wrapper to the hight of the next element, then fade that element in
+			setTimeout(function() {
+				$(quote).parent().animate({height: wrapHeight}, speed);
+			}, speed);
+			
+			if($(lastQuo).is(':visible')) {
+				setTimeout(function() {
+					$(firstQuo).fadeIn(speed*2);
+				}, speed*2);
+				
+			} else {
+				setTimeout(function() {
+					$(nextElem).fadeIn(speed);
+				}, speed*2);
+			}
+			
+		}, delay);
+	
+	};
+})(jQuery);
--- js/scroller.js
+++ js/scroller.js
@@ -4,142 +4,138 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- *
+ * 
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
-;
-(function (window) {
+;( function( window ) {
+	
+	'use strict';
 
-    'use strict';
+	var docElem = window.document.documentElement;
 
-    var docElem = window.document.documentElement;
+	function getViewportH() {
+		var client = docElem['clientHeight'],
+			inner = window['innerHeight'];
+		
+		if( client < inner )
+			return inner;
+		else
+			return client;
+	}
 
-    function getViewportH () {
-        var client = docElem['clientHeight'],
-            inner = window['innerHeight'];
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
 
-        if (client < inner)
-            return inner;
-        else
-            return client;
-    }
+	// http://stackoverflow.com/a/5598797/989439
+	function getOffset( el ) {
+		var offsetTop = 0, offsetLeft = 0;
+		do {
+			if ( !isNaN( el.offsetTop ) ) {
+				offsetTop += el.offsetTop;
+			}
+			if ( !isNaN( el.offsetLeft ) ) {
+				offsetLeft += el.offsetLeft;
+			}
+		} while( el = el.offsetParent )
 
-    function scrollY () {
-        return window.pageYOffset || docElem.scrollTop;
-    }
+		return {
+			top : offsetTop,
+			left : offsetLeft
+		}
+	}
 
-    // http://stackoverflow.com/a/5598797/989439
-    function getOffset (el) {
-        var offsetTop = 0, offsetLeft = 0;
-        do {
-            if (!isNaN (el.offsetTop)) {
-                offsetTop += el.offsetTop;
-            }
-            if (!isNaN (el.offsetLeft)) {
-                offsetLeft += el.offsetLeft;
-            }
-        } while (el = el.offsetParent)
+	function inViewport( el, h ) {
+		var elH = el.offsetHeight,
+			scrolled = scrollY(),
+			viewed = scrolled + getViewportH(),
+			elTop = getOffset(el).top,
+			elBottom = elTop + elH,
+			// if 0, the element is considered in the viewport as soon as it enters.
+			// if 1, the element is considered in the viewport only when it's fully inside
+			// value in percentage (1 >= h >= 0)
+			h = h || 0;
 
-        return {
-            top : offsetTop,
-            left: offsetLeft
-        }
-    }
+		return (elTop + elH * h) <= viewed && (elBottom) >= scrolled;
+	}
 
-    function inViewport (el, h) {
-        var elH = el.offsetHeight,
-            scrolled = scrollY (),
-            viewed = scrolled + getViewportH (),
-            elTop = getOffset (el).top,
-            elBottom = elTop + elH,
-        // if 0, the element is considered in the viewport as soon as it enters.
-        // if 1, the element is considered in the viewport only when it's fully inside
-        // value in percentage (1 >= h >= 0)
-            h = h || 0;
+	function extend( a, b ) {
+		for( var key in b ) { 
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
 
-        return (elTop + elH * h) <= viewed && (elBottom) >= scrolled;
-    }
+	function cbpScroller( el, options ) {	
+		this.el = el;
+		this.options = extend( this.defaults, options );
+		this._init();
+	}
 
-    function extend (a, b) {
-        for (var key in b) {
-            if (b.hasOwnProperty (key)) {
-                a[key] = b[key];
-            }
-        }
-        return a;
-    }
+	cbpScroller.prototype = {
+		defaults : {
+			// The viewportFactor defines how much of the appearing item has to be visible in order to trigger the animation
+			// if we'd use a value of 0, this would mean that it would add the animation class as soon as the item is in the viewport. 
+			// If we were to use the value of 1, the animation would only be triggered when we see all of the item in the viewport (100% of it)
+			viewportFactor : 0.6
+		},
+		_init : function() {
+			if( Modernizr.touch ) return;
+			this.sections = Array.prototype.slice.call( this.el.querySelectorAll( '.cbp-so-section' ) );
+			this.didScroll = false;
 
-    function cbpScroller (el, options) {
-        this.el = el;
-        this.options = extend (this.defaults, options);
-        this._init ();
-    }
+			var self = this;
+			// the sections already shown...
+			this.sections.forEach( function( el, i ) {
+				if( !inViewport( el ) ) {
+					classie.add( el, 'cbp-so-init' );
+				}
+			} );
 
-    cbpScroller.prototype = {
-        defaults   : {
-            // The viewportFactor defines how much of the appearing item has to be visible in order to trigger the animation
-            // if we'd use a value of 0, this would mean that it would add the animation class as soon as the item is in the viewport.
-            // If we were to use the value of 1, the animation would only be triggered when we see all of the item in the viewport (100% of it)
-            viewportFactor: 0.6
-        },
-        _init      : function () {
-            if (Modernizr.touch) return;
-            this.sections = Array.prototype.slice.call (this.el.querySelectorAll ('.cbp-so-section'));
-            this.didScroll = false;
+			var scrollHandler = function() {
+					if( !self.didScroll ) {
+						self.didScroll = true;
+						setTimeout( function() { self._scrollPage(); }, 60 );
+					}
+				},
+				resizeHandler = function() {
+					function delayed() {
+						self._scrollPage();
+						self.resizeTimeout = null;
+					}
+					if ( self.resizeTimeout ) {
+						clearTimeout( self.resizeTimeout );
+					}
+					self.resizeTimeout = setTimeout( delayed, 200 );
+				};
 
-            var self = this;
-            // the sections already shown...
-            this.sections.forEach (function (el, i) {
-                if (!inViewport (el)) {
-                    classie.add (el, 'cbp-so-init');
-                }
-            });
+			window.addEventListener( 'scroll', scrollHandler, false );
+			window.addEventListener( 'resize', resizeHandler, false );
+		},
+		_scrollPage : function() {
+			var self = this;
 
-            var scrollHandler = function () {
-                    if (!self.didScroll) {
-                        self.didScroll = true;
-                        setTimeout (function () {
-                            self._scrollPage ();
-                        }, 60);
-                    }
-                },
-                resizeHandler = function () {
-                    function delayed () {
-                        self._scrollPage ();
-                        self.resizeTimeout = null;
-                    }
+			this.sections.forEach( function( el, i ) {
+				if( inViewport( el, self.options.viewportFactor ) ) {
+					classie.add( el, 'cbp-so-animate' );
+				}
+				else {
+					// this add class init if it doesn't have it. This will ensure that the items initially in the viewport will also animate on scroll
+					classie.add( el, 'cbp-so-init' );
+					classie.remove( el, 'cbp-so-animate' );
+				}
+			});
+			this.didScroll = false;
+		}
+	}
 
-                    if (self.resizeTimeout) {
-                        clearTimeout (self.resizeTimeout);
-                    }
-                    self.resizeTimeout = setTimeout (delayed, 200);
-                };
+	// add to global namespace
+	window.cbpScroller = cbpScroller;
 
-            window.addEventListener ('scroll', scrollHandler, false);
-            window.addEventListener ('resize', resizeHandler, false);
-        },
-        _scrollPage: function () {
-            var self = this;
-
-            this.sections.forEach (function (el, i) {
-                if (inViewport (el, self.options.viewportFactor)) {
-                    classie.add (el, 'cbp-so-animate');
-                }
-                else {
-                    // this add class init if it doesn't have it. This will ensure that the items initially in the viewport will also animate on scroll
-                    classie.add (el, 'cbp-so-init');
-                    classie.remove (el, 'cbp-so-animate');
-                }
-            });
-            this.didScroll = false;
-        }
-    }
-
-    // add to global namespace
-    window.cbpScroller = cbpScroller;
-
-}) (window);
+} )( window );
 
 /*!
  * classie - class helper functions
@@ -154,70 +150,70 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-(function (window) {
+( function( window ) {
 
-    'use strict';
+'use strict';
 
 // class helper functions from bonzo https://github.com/ded/bonzo
 
-    function classReg (className) {
-        return new RegExp ("(^|\\s+)" + className + "(\\s+|$)");
-    }
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
 
 // classList support for class management
 // altho to be fair, the api sucks because it won't accept multiple classes at once
-    var hasClass, addClass, removeClass;
+var hasClass, addClass, removeClass;
 
-    if ('classList' in document.documentElement) {
-        hasClass = function (elem, c) {
-            return elem.classList.contains (c);
-        };
-        addClass = function (elem, c) {
-            elem.classList.add (c);
-        };
-        removeClass = function (elem, c) {
-            elem.classList.remove (c);
-        };
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
     }
-    else {
-        hasClass = function (elem, c) {
-            return classReg (c).test (elem.className);
-        };
-        addClass = function (elem, c) {
-            if (!hasClass (elem, c)) {
-                elem.className = elem.className + ' ' + c;
-            }
-        };
-        removeClass = function (elem, c) {
-            elem.className = elem.className.replace (classReg (c), ' ');
-        };
-    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
 
-    function toggleClass (elem, c) {
-        var fn = hasClass (elem, c) ? removeClass : addClass;
-        fn (elem, c);
-    }
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
 
-    var classie = {
-        // full names
-        hasClass   : hasClass,
-        addClass   : addClass,
-        removeClass: removeClass,
-        toggleClass: toggleClass,
-        // short names
-        has        : hasClass,
-        add        : addClass,
-        remove     : removeClass,
-        toggle     : toggleClass
-    };
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
 
 // transport
-    if (typeof define === 'function' && define.amd) {
-        // AMD
-        define (classie);
-    } else {
-        // browser global
-        window.classie = classie;
-    }
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
 
-}) (window);
+})( window );
